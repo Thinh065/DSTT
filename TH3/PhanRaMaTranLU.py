bc0841f03bcf083bc3bf20286c9cec4ca1480105
Thinh065 <trannhatthinh2004@gmail.com>
2026-01-21 17:05:55 +0700

    cập nhật lại, chuyển từ file py sang ipynb

--- TH3/PhanRaMaTranLU.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from scipy import linalg
-M = np.array([ [0.8, 0.3], [0.2, 0.7]])
-P, L, U = linalg.lu(M) # lệnh lina.lu sẽ tách ma trận M thành 3 ma trận P, L và U
-print("Ma trận P:\n", P)
-print("Ma trận L:\n", L)
-print("Ma trận U:\n", U)
